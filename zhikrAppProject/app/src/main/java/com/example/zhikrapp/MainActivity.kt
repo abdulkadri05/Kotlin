fff8b7baf278b6c5e6dc11c4ae7528d5ef23db82
Abdul Al kadri <abdul.al.kadri@outlook.com>
2024-07-29 16:46:19 +0200

    newProject

---
+++ zhikrAppProject/app/src/main/java/com/example/zhikrapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.zhikrapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.zhikrapp.App.MainScreen
+import com.example.zhikrapp.ui.theme.ZhikrAppTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ZhikrAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
